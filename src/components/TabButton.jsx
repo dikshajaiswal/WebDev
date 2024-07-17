a4e93d94d31a8384a1004a08f5c0c3a1b7aceeb1
Diksha Jaiswal <dikshajaiswal111@gmail.com>
2024-07-17 15:14:44 -0400

    React: props: forwarding props to components
    Signed-off-by: Diksha Jaiswal <dikshajaiswal111@gmail.com>

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,11 +1,11 @@
-export default function TabButton({children, onSelect, isSelected}){
+export default function TabButton({children, isSelected, ...props}){
     function handleClick(){
         console.log('Hello World!');
     }
     console.log('TabButton Component executing');
     return (
         <li>
-            <button className={isSelected ? 'active' : undefined} onClick={onSelect}>{children}</button>
+            <button className={isSelected ? 'active' : undefined} {...props}>{children}</button>
         </li>
     );
 }
