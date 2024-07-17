f62392ffd4a706f6bbde61e40fd1cc3b0f7079e8
Diksha Jaiswal <dikshajaiswal111@gmail.com>
2024-07-16 23:28:36 -0400

    React: Components: Split components (Lecture - 64)
    https://www.udemy.com/course/react-the-complete-guide-incl-redux/learn/lecture/39659748#overview
    
    Signed-off-by: Diksha Jaiswal <dikshajaiswal111@gmail.com>

--- src/components/CoreConcepts.jsx
+++ src/components/CoreConcepts.jsx
@@ -1,9 +1,19 @@
-export default function CoreConcept({image, title, description}){
-    return (
-      <li>
-        <img src={image} alt="..." />
-        <h3>{title}</h3>
-        <p>{description}</p>
-      </li>
-    );
-  }
+import CoreConcept from "./CoreConcept";
+import { CORE_CONCEPTS } from "../data";
+
+export default function CoreConcepts(){
+    return         (<section id = 'core-concepts'>
+    <h2> Core Concepts </h2>
+    <ul>
+      {CORE_CONCEPTS.map((conceptItem) => (<CoreConcept {...conceptItem} />))}
+      {/* <CoreConcept
+        title={CORE_CONCEPTS[0].title}
+        description={CORE_CONCEPTS[0].description}
+        image={CORE_CONCEPTS[0].image}
+      />
+      <CoreConcept {...CORE_CONCEPTS[1]} />
+      <CoreConcept {...CORE_CONCEPTS[2]}/>
+      <CoreConcept {...CORE_CONCEPTS[3]}/> */}
+    </ul>
+  </section>);
+}
---
+++ src/components/Examples.jsx
@@ -0,0 +1,42 @@
+import TabButton from './TabButton.jsx';
+import { EXAMPLES } from '../data.js';
+import { useState} from 'react';
+import Section from './Section.jsx';
+
+export default function Examples(){
+
+  // let tabContent = "Please click a button";
+  const [selectedTopic, setSelectedTopic] = useState('');
+
+  function handleSelect(selectedButton){
+    // console.log('Hello World - selected!');
+    setSelectedTopic(selectedButton);
+    // tabContent = selectedButton;
+    console.log(selectedTopic);
+  }
+
+  let tabContent = <p> Please select a topic.</p>;
+
+  if (selectedTopic) {
+    tabContent = <div id="tab-content">
+    <h3>{EXAMPLES[selectedTopic].title}</h3>
+    <p>{EXAMPLES[selectedTopic].description}</p>
+    <pre>
+      <code>
+      {EXAMPLES[selectedTopic].code}
+      </code>
+    </pre>
+  </div>;
+
+  }
+
+    return (<Section title="Examples" id="examples">
+    <menu>
+      <TabButton isSelected={selectedTopic === 'components'} onSelect={() => handleSelect('components')}>Components</TabButton>
+      <TabButton isSelected={selectedTopic === 'jsx'} onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+      <TabButton isSelected={selectedTopic === 'props'} onSelect={() => handleSelect('props')}>Props</TabButton>
+      <TabButton isSelected={selectedTopic === 'state'} onSelect={() => handleSelect('state')}>State</TabButton>
+    </menu>
+      {tabContent}
+  </Section>);
+}
